fa0a87d24cb0e3c02576d4131c65f62c5c655522
Liss <lissj014@gmail.com>
2024-05-08 16:10:51 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace segundoProjeto
+{
+    class Projeto
+    {
+        static void Main(string[] args)
+        {
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 1------------------------*/
+
+            /*int quantidadeSorvete = 26;
+
+            if (quantidadeSorvete < 0)
+            {
+                Console.WriteLine("Não há como comprar algo negativo.");
+            }
+            else if (quantidadeSorvete <= 25)
+            {
+                double totalPagarMenor = quantidadeSorvete * 1.50;
+
+                Console.WriteLine("Você irá pagar R$" + totalPagarMenor);
+            }
+            else
+            {
+                double totalPagarMaior = quantidadeSorvete * 1.25;
+
+                Console.WriteLine("Você irá pagar R$" + totalPagarMaior);
+            }*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 2------------------------*/
+
+            /*int num = 9;
+
+            if (num >= 10)
+            {
+                float total = (num + 10) * 2;
+
+                Console.WriteLine("O total da conta é " + total);
+            }
+            else
+            {
+                float total2 = (num + 9) * (3 - 8);
+
+                Console.WriteLine("O total da conta é " + total2);
+            }*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 3------------------------*/
+
+            /*int num = -5;
+
+            if (num < 0)
+            {
+                Console.WriteLine("O número é negativo.");
+            } else if (num == 0)
+            {
+                Console.WriteLine("O número é igual a zero.");
+            }
+            else
+            {
+                Console.WriteLine("O número é positivo.");
+            }*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 4------------------------*/
+
+            /*float num1 = 5;
+            float num2 = 3;
+
+            if(num1 < num2)
+            {
+                Console.WriteLine("O número " + num1 + " é menor do que o número " + num2 + ".");
+            } else if (num1 == num2)
+            {
+                Console.WriteLine("O número " + num1 + " é igual ao número " + num2 + ".");
+            }
+            else
+            {
+                Console.WriteLine("O número " + num1 + " é maior do que o número " + num2 + ".");
+            }*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 5------------------------*/
+
+            /*double notaUm = 7;
+            double notaDois = 2.5;
+
+            double media = (notaUm + notaDois) / 2;
+
+            if(media >= 7)
+            {
+                Console.WriteLine("Aprovado. Parabéns!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado.");
+            }*/
+        }
+    }
+}
